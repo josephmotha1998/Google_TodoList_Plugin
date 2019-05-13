8a916047b1e41a8892aba8ac885ade616eb2e991
Joseph Motha <josephmotha@outlook.com>
2019-05-13 16:56:12 -0400

    upadted funcitonality of the todo list

---
+++ Todolistproject/Assets/js/todos.js
@@ -0,0 +1,25 @@
+// check of todos by clicking
+
+$("ul").on("click","li",function(){
+//if li is grey then turn it black, else turn it grey;
+	$(this).toggleClass("completed");
+});
+
+// click on x to delete todo
+
+$("ul").on("click","span",function(e){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e ){
+	if(e.which===13){
+		//grabbing tdo textfrom input
+		var todoText=$(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> "+todoText+"</li>")
+	}
+});
